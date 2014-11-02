3ac568f93d0db8030db0db6ed691f9531c849d4d
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-11-02 11:38:51 -0800

    refactor go_random and add more LAlocal roasteries

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,18 +1,6 @@
 class RoastersController < ApplicationController
   def go_random
-    urls = [
-      'http://wildgoosecoffee.com/',
-      'http://www.groundworkcoffee.com/',
-      'http://www.thebestcoffee.com/',
-      'http://www.jamesonbrown.com/',
-      'http://www.portolacoffeelab.com/',
-      'http://barninela.com/',
-      'http://10speedcoffee.com/',
-      'http://liftcoffeeroasters.com/',
-      'http://truebeans.com/',
-      'http://lordwindsor.com/',
-      'http://www.roseparkroasters.com/'
-    ]
-    redirect_to urls[rand(urls.length)]
+    yml_file_path = Rails.root.join("lib","tasks","roasters","urls.yml")
+    redirect_to YAML.load_file(yml_file_path).sample
   end
 end
