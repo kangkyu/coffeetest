8e49fb521afc8e6a531bf74cfe26871911a48934
kangkyu <kangkyu1111@gmail.com>
2014-11-29 17:30:42 -0800

    clean controller and change bootstrap files

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -5,12 +5,7 @@ class RoastersController < ApplicationController
   end
 
   def show_random_screen
-    yml_file_path = Rails.root.join("lib","tasks","roasters","urls.yml")
+    yml_file_path = Rails.root.join("lib/tasks/roasters/urls.yml")
     @screen = YAML.load_file(yml_file_path).sample
-
-    # render text: "
-    # <head><style>h1 {font-family:verdana}</style></head>
-    # <body><h1><%= @screen %></h1></body>
-    # "
   end
 end
