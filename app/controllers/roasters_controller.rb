dde3b96dc37123a985ba5eb33f41b378165eb370
kangkyu <kangkyu1111@gmail.com>
2014-12-04 09:05:28 -0800

    edit roaster list yml as with nested entries

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,11 +1,11 @@
 class RoastersController < ApplicationController
   def go_random
-    yml_file_path = Rails.root.join("lib","tasks","roasters","urls.yml")
-    redirect_to YAML.load_file(yml_file_path).sample
+    yml_file_path = Rails.root.join("lib","tasks","roasters","roasters.yml")
+    redirect_to YAML.load_file(yml_file_path).sample["url"]
   end
 
   def show_random_screen
-    yml_file_path = Rails.root.join("lib/tasks/roasters/urls.yml")
-    @screen = YAML.load_file(yml_file_path).sample
+    yml_file_path = Rails.root.join("lib/tasks/roasters/roasters.yml")
+    @screen = YAML.load_file(yml_file_path).sample["url"]
   end
 end
