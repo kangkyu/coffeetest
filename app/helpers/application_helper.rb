ee0f8ffd132dd3f21ee421973145021474e4ae47
kangkyu <kangkyu1111@gmail.com>
2015-07-28 04:41:33 -0700

    arrange the coffee cards on index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,11 @@ module ApplicationHelper
     asset_url screen["url"].slice(/:.*/).gsub(/[^a-zA-Z0-9]/, "") + "-clipped.png"
   end
 
+  # http://stackoverflow.com/questions/980855/inputting-a-default-image-in-case-the-src-arribute-of-an-html-img-is-not-valid
+
   def command_take_picture(screen)
     "webkit2png " + screen["url"] + " --scale=1.0 --clipped --clipwidth=640 --clipheight=480"
   end
+
+  # http://www.paulhammond.org/webkit2png/
 end
